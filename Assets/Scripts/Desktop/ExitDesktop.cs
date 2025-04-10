0dec38c54a99d2e4251e595e3abe868b5a334197
jaeda <jaedaramirez@gmail.com>
2025-04-10 23:12:35 +0800

    Changed Power Button to Return to Scene

--- Assets/Scripts/Desktop/ExitDesktop.cs
+++ Assets/Scripts/Desktop/ExitDesktop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ExitDesktop : MonoBehaviour
@@ -8,6 +9,7 @@ public class ExitDesktop : MonoBehaviour
     public Image img;
     public Sprite newImg;
     public Sprite oldImg;
+    public string scene;
 
     void Start()
     {
@@ -24,6 +26,16 @@ public class ExitDesktop : MonoBehaviour
         Invoke("Exit", 0.6f);
     }
 
+    public void ReturnToScene()
+    {
+        SoundFX.Play("PowerButton");
+
+        img.sprite = newImg;
+        StartCoroutine(RevertImage());
+
+        Invoke("Return", 0.6f);
+    }
+
     private IEnumerator RevertImage()
     {
         yield return new WaitForSeconds(0.3f);
@@ -37,5 +49,10 @@ public class ExitDesktop : MonoBehaviour
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+
+    private void Return()
+    {
+        SceneManager.LoadScene(scene);
     }
 }
